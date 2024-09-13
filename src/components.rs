58fffeaa99a42dd5faba50fb1b8aad789ca9594a
Yevhen Arteshchuk <jin992@Yevhens-MacBook-Pro.local>
2024-09-13 10:44:15 +0200

    init commit

---
+++ src/components.rs
@@ -0,0 +1,17 @@
+
+use bevy::prelude::*;
+use serde::{Deserialize, Serialize};
+use bevy_replicon::prelude::*;
+/// Contains the client ID of a player.
+#[derive(Component, Serialize, Deserialize)]
+pub struct Player(ClientId);
+
+#[derive(Component, Deserialize, Serialize, Deref, DerefMut)]
+pub struct PlayerPosition(Vec2);
+
+#[derive(Component, Deserialize, Serialize)]
+pub struct PlayerColor(Color);
+
+/// A movement event for the controlled box.
+#[derive(Debug, Default, Deserialize, Event, Serialize)]
+pub struct MoveDirection(Vec2);
