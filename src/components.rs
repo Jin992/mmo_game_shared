8548a180a6f8687a2221d7855a115f1554761315
Yevhen Arteshchuk <jin992@Yevhens-MacBook-Pro.local>
2024-09-13 11:37:09 +0200

    refactoring, removed tuples

--- src/components.rs
+++ src/components.rs
@@ -4,26 +4,24 @@ use serde::{Deserialize, Serialize};
 use bevy_replicon::prelude::*;
 /// Contains the client ID of a player.
 #[derive(Component, Serialize, Deserialize)]
-pub struct Player(ClientId);
-
-impl Player {
-    pub fn client_id(&self) -> ClientId {
-        self.0
-    }
+pub struct Player {
+   pub client_id: ClientId
 }
 
+
+
 #[derive(Component, Deserialize, Serialize, Deref, DerefMut)]
-pub struct PlayerPosition(Vec2);
+pub struct PlayerPosition {
+    pub position: Vec3,
+}
 
 #[derive(Component, Deserialize, Serialize)]
-pub struct PlayerColor(Color);
+pub struct PlayerColor {
+    pub color: Color,
+}
 
 /// A movement event for the controlled box.
 #[derive(Debug, Default, Deserialize, Event, Serialize)]
-pub struct MoveDirection(Vec2);
-
-impl MoveDirection {
-    pub fn direction(&self) -> Vec2 {
-        self.0
-    }
+pub struct MoveDirection {
+    pub direction: Vec2,
 }
